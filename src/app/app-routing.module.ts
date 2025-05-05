e0b2f7acaf6839ad754de4b1d76f99b5750aae40
Mariana <m.kanashiro@iesb.edu.br>
2025-05-05 17:59:34 -0300

    HTML e CSS para componente TECNICO-CREATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
-import { AuthGuard } from './auth/auth.guard';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [

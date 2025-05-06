8ea1e13911607ed12f18f40aa79dd577327ef151
Mariana <m.kanashiro@iesb.edu.br>
2025-05-06 17:51:11 -0300

    filtro e metodo findAll para CHAMADO

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Chamado } from 'src/app/models/chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-list',
@@ -11,34 +12,89 @@ import { Chamado } from 'src/app/models/chamado';
 export class ChamadoListComponent implements OnInit {
 
   ELEMENT_DATA: Chamado[] = [
-      {
-        id: 1,
-        dataAbertura: '21/06/2021',
-        dataFechamento: '15/02/2022',
-        prioridade: 'ALTA',
-        status: 'ANDAMENTO',
-        titulo: 'chamado 1',
-        descricao: 'chamado 1 ein',
-        tecnico: 1,
-        cliente: 2,
-        nomeCliente: 'Mariana Kanashiro',
-        nomeTecnico: 'Valdir'
-      }
-    ]
+    {
+      id: 1,
+      dataAbertura: '21/06/2021',
+      dataFechamento: '15/02/2022',
+      prioridade: '1',
+      status: '2',
+      titulo: 'chamado 1',
+      descricao: 'chamado 1 ein',
+      tecnico: 1,
+      cliente: 2,
+      nomeCliente: 'Mariana Kanashiro',
+      nomeTecnico: 'Valdir'
+    },
+    {
+      id: 2,
+      dataAbertura: '31/07/2022',
+      dataFechamento: '25/05/2024',
+      prioridade: '0',
+      status: '1',
+      titulo: 'chamado 2',
+      descricao: 'chamado 2 ein',
+      tecnico: 2,
+      cliente: 1,
+      nomeCliente: 'Valdir',
+      nomeTecnico: 'Mariana Kanashiro'
+    }
+  ]
+  FILTERED_DATA: Chamado[] = []
 
-    displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
-    dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
-  
-    @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private service: ChamadoService) { }
 
   ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatus(status: any): string {
+    if(status === '0') {
+      return 'ABERTO';
+    } else if (status === '1') {
+      return 'EM ANDAMENTO';
+    } else {
+      return 'ENCERRADO';
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade === '0') {
+      return 'BAIXA';
+    } else if (prioridade === '1') {
+      return 'MÉDIA';
+    } else {
+      return 'ALTA';
+    }
+  }
+
+  orderByStatus(status: any): void {
+    let list: Chamado[] = []
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.status === status) {
+        list.push(element);
+      }
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(list);
+    this.dataSource.paginator = this.paginator;
   }
 
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -11,8 +11,8 @@ import { AuthService } from 'src/app/services/auth.service';
 export class NavComponent implements OnInit {
 
   constructor(private router: Router,
-              private authService: AuthService,
-              private toast: ToastrService) { }
+    private authService: AuthService,
+    private toast: ToastrService) { }
 
   ngOnInit(): void {
     this.router.navigate(['chamados'])
@@ -21,6 +21,6 @@ export class NavComponent implements OnInit {
   logout() {
     this.router.navigate(['login'])
     this.authService.logout()
-    this.toast.info('Logout realizado com sucesso', 'Logout', {timeOut: 7000})
+    this.toast.info('Logout realizado com sucesso', 'Logout', { timeOut: 7000 })
   }
 }

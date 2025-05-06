62ea4548dd5384d314ad0742fdcfb3f3fcd7a210
Mariana <m.kanashiro@iesb.edu.br>
2025-05-06 14:10:08 -0300

    componente tecnico-update implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
   {
@@ -15,7 +16,8 @@ const routes: Routes = [
       { path: 'home', component: HomeComponent },
 
       { path: 'tecnicos', component: TecnicoListComponent },
-      { path: 'tecnicos/create', component: TecnicoCreateComponent }
+      { path: 'tecnicos/create', component: TecnicoCreateComponent },
+      { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent }
     ]
   }
 ];
